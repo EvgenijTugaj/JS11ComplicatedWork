8e6ee7575191539680f1f7b88b33c7b601da18e9
Evgenij Tugaj <79255220650@ya.ru>
2020-01-27 15:56:04 +0300

    Урок 3

---
+++ app.js
@@ -0,0 +1,23 @@
+let lang = 'en';
+let days = {
+    ru : ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскрессенье'],
+    en : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+
+if( lang === 'ru' || lang === 'en')
+    console.log(String(days[lang]));
+
+switch(true){
+    case lang === 'ru' || lang === 'en':
+        console.log(String(days[lang]));
+        break;
+    default:
+        console.log('Не "ru" и не "en"');    
+}
+
+for (let i = 0; i < days[lang].length; i++) {
+    console.log(days[lang][i]);
+    }
+
+let namePerson = 'Артём';
+console.log(namePerson === 'Артём' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент');
